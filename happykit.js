b18c5348f8829c10517243d7d4c6d4854ffb9ac5
Vinay <vinaywebguy@gmail.com>
2023-09-12 12:43:44 +0530

    Added Loader 3 and created hide loader function

--- happykit.js
+++ happykit.js
@@ -1,10 +1,7 @@
 let Loaders = {
     loader1: `<div class="hk_loader" id="hk_loader-1"></div>`,
-    loader2: `<div class="hk_loader" id="hk_loader-2">
-                <span id="hk_loader-2_1" class="loader-span" style="--i: 0.3s;"></span>
-                <span id="hk_loader-2_2" class="loader-span" style="--i: 0.7s;"></span>
-                <span id="hk_loader-2_3" class="loader-span" style="--i: 1.1s;"></span>
-            </div>`
+    loader2: `<div class="hk_loader" id="hk_loader-2"><span id="hk_loader-2_1" class="loader-span" style="--i: 0.3s;"></span><span id="hk_loader-2_2" class="loader-span" style="--i: 0.7s;"></span><span id="hk_loader-2_3" class="loader-span" style="--i: 1.1s;"></span></div>`,
+    loader3: `<div id="hk_loader-3" class="hk_loader"><span></span></div>`
 }
 
 
@@ -15,32 +12,60 @@ let Loader1 = {
         $('#hk_loader-1').css({
             height: dimensions[0],
             width: dimensions[1],
-            border: obj.stroke + 'px solid ' + obj.color,
-            animation: 'loader1animation ' + obj.duration * 100 + 'ms linear infinite'
+            border: (obj.stroke ? obj.stroke : 2) + 'px solid ' + (obj.color ? obj.color : '#000'),
+            animation: 'loader1animation ' + (obj.duration ? obj.duration : 7) * 100 + 'ms linear infinite'
         });
         $('#hk_loader-1').addClass('show');
+    },
+    hide: function () {
+        $('#hk_loader-1').removeClass('show');
     }
 }
 let Loader2 = {
     show: function (obj) {
         $(`#${obj.where}`).html(Loaders.loader2)
         let dimensions = obj.dimensions;
+        let speed = (obj.initialSpeed ? obj.initialSpeed :4);
         $('#hk_loader-2').css({
             height: dimensions[0],
             width: dimensions[1],
         });
         $('#hk_loader-2_1').css({
-            background: obj.colors[0],
-            animation: `loader2animation ${(obj.initialSpeed) * 100}ms linear infinite alternate`,
+            background: (obj.colors[0] ? obj.colors[0] : "red"),
+            animation: `loader2animation ${(speed) * 100}ms linear infinite alternate`,
         })
         $('#hk_loader-2_2').css({
-            background: obj.colors[1],
-            animation: `loader2animation ${(obj.initialSpeed + (obj.initialSpeed / 4)) * 100}ms linear infinite alternate`,
+            background: (obj.colors[1] ? obj.colors[1] : "green"),
+            animation: `loader2animation ${(speed + (speed / 4)) * 100}ms linear infinite alternate`,
         })
         $('#hk_loader-2_3').css({
-            background: obj.colors[2],
-            animation: `loader2animation ${(obj.initialSpeed + (obj.initialSpeed / 2)) * 100}ms linear infinite alternate`,
+            background:( obj.colors[2] ? obj.colors[2] : "blue"),
+            animation: `loader2animation ${(speed + (speed / 2)) * 100}ms linear infinite alternate`,
         })
         $('#hk_loader-2').addClass('show');
+    },
+    hide: function () {
+        $('#hk_loader-2').removeClass('show');
+    }
+}
+
+let Loader3 = {
+    show: function (obj) {
+        $(`#${obj.where}`).html(Loaders.loader3)
+        let dimensions = obj.dimensions;
+        $('#hk_loader-3').css({
+            height: dimensions[0],
+            width: dimensions[1],
+            background: (obj.color ? obj.color : "#000"),
+        });
+        $('#hk_loader-3 span').css({
+            background: (obj.loadingColor ? obj.loadingColor : "#fff"),
+            width: (obj.initialProgress ? obj.initialProgress : 10) + '%',
+            animation: `loader3animation ${obj.speed*100}ms linear 0.7s forwards`
+        })
+        $('#hk_loader-3').addClass('show');
+    },
+    hide: function () {
+        $('#hk_loader-3').removeClass('show');
     }
 }
--- happykit_config.js
+++ happykit_config.js
@@ -1,17 +1,29 @@
 Loader1.show({
-    where : "l1",
-    dimensions : ["50px", "50px"],
-    color : "#008aff",
-    stroke : 5,
-    duration : 7
+    where: "l1",
+    dimensions: ["50px", "50px"],
+    color: "#008aff",
+    stroke: 5,
+    duration: 7
 });
 
 
 Loader2.show({
-    where : "l2",
-    dimensions : ["50px", "80px"],
-    colors : ["#000", "#0f0", "#f00"],
-    initialSpeed : 5
+    where: "l2",
+    dimensions: ["50px", "80px"],
+    colors: ["#212121", "#0f0", "#ccc"],
+    initialSpeed: 5
+})
+
+Loader3.show({
+    where: "l3",
+    dimensions: ["10px", "200px"],
+    color: "#00f",
+    loadingColor: "#f00",
+    // initialProgress : 0,
+    speed: 30,
 })
 
 
+Loader1.hide();
+Loader2.hide();
+Loader3.hide();
